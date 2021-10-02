e39ac860de3eadcbeeb0336d943d35e175f1af55
Arunvel Sriram <arunvelsriram@gmail.com>
2021-10-03 01:28:30 +0530

    Handle edge cases and form submit

--- src/components/FindExplanationQuiz.js
+++ src/components/FindExplanationQuiz.js
@@ -1,25 +1,54 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Button, Container, Form } from "react-bootstrap"
 import { getExplanations } from "../service/Quiz"
 import { getRandomKural } from "../service/Thirukural"
 
 const FindExplanationQuiz = () => {
-  const [kural, setKural] = useState(getRandomKural())
-  const [explanations, setExplanations] = useState(getExplanations(kural))
+  const [kural, setKural] = useState(null)
+  const [explanations, setExplanations] = useState([])
+  const [selectedExplanationIdx, setSelectedExplanationIdx] = useState(null)
+
+  useEffect(() => {
+    if (kural === null && explanations.length === 0) {
+      const kural = getRandomKural()
+      const explanations = getExplanations(kural)
+      setKural(kural)
+      setExplanations(explanations)
+    }
+  }, [kural, explanations])
+
+  const handleOnSubmit = (e) => {
+    const selectedExplanation = explanations[selectedExplanationIdx].explanation
+    const correctExplanation = kural.explanations[0].explanation
+    console.log(`handle form submit, selectedExplanation: ${selectedExplanation} correctExplanation: ${correctExplanation}`)
+    if (selectedExplanation === correctExplanation) {
+      console.log("correct option chosen")
+    } else {
+      console.log("wrong option chosen")
+    }
+    e.preventDefault()
+  }
 
   return (
     <Container>
-      <Form>
+      <Form onSubmit={handleOnSubmit}>
         <Form.Group>
-          <Form.Label className="kural">{`${kural.kuralNo}: ${kural.kural}`}</Form.Label>
+          {
+            kural !== null ?
+              <Form.Label className="kural">{`${kural.kuralNo}: ${kural.kural}`}</Form.Label>
+              : <Form.Label className="kural"></Form.Label>
+          }
         </Form.Group>
         <Form.Group>
           {
             explanations.map((item, idx) => (
               <Form.Check
                 key={idx}
+                value={idx}
+                name="explanations"
                 type="radio"
                 label={item.explanation}
+                onChange={(e) => setSelectedExplanationIdx(e.target.value)}
               />
             ))
           }
