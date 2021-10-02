c7b478a4fe805adde0c78c6e57939ba2d427f9eb
Arunvel Sriram <arunvelsriram@gmail.com>
2021-10-02 19:21:26 +0530

    Hide footer for now as it has nothing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,10 @@
-import { Container, Navbar, NavbarBrand } from "react-bootstrap"
+import { Container, Navbar } from "react-bootstrap"
 
 const Footer = () => {
   return (
     <footer className="fixed-bottom">
-      <Navbar variant="dark" bg="secondary">
+      <Navbar variant="light" bg="light">
         <Container>
-          <NavbarBrand>Footer</NavbarBrand>
         </Container>
       </Navbar>
     </footer>
