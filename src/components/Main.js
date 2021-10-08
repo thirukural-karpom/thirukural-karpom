818c2bfa8fad012fb344807f74a5136b02ea5823
Arunvel Sriram <arunvelsriram@gmail.com>
2021-10-09 02:09:44 +0530

    Remove homepage

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { Switch, Route } from "react-router-dom"
-import Home from "./Home"
 import Kurals from "./Kurals"
 import FindExplanationQuiz from "./FindExplanationQuiz"
 import FindKuralQuiz from "./FindKuralQuiz"
@@ -8,8 +7,7 @@ import FindKuralQuiz from "./FindKuralQuiz"
 const Main = () => (
   <main>
     <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/kurals" component={Kurals} />
+      <Route exact path="/" component={Kurals} />
       <Route exact path="/quiz/findExplanation" component={FindExplanationQuiz} />
       <Route exact path="/quiz/findKural" component={FindKuralQuiz} />
     </Switch>
